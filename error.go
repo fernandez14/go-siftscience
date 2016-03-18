2928465ee4197263b74da9f2714ba91907059c5d
fernandez14 <fernandez14@outlook.com>
2016-03-18 13:09:08 -0600

    [add] Generic sift science Track

---
+++ error.go
@@ -0,0 +1,16 @@
+package gosift
+
+import (
+	"fmt"
+)
+
+type SiftError struct {
+	Status       int
+	ErrorMessage string `json:"error_message"`
+	Time         int64
+	Request      string
+}
+
+func (e *SiftError) Error() string {
+	return fmt.Sprintf("%d - %s", e.Status, e.ErrorMessage)
+}
---
+++ main.go
@@ -0,0 +1,47 @@
+package gosift
+
+import (
+	"gopkg.in/jmcvetta/napping.v3"
+
+	"fmt"
+)
+
+const (
+	siftEndpoint = "https://api.siftscience.com/v203/events"
+)
+
+var ApiKey string
+var UserId string
+var SessionId string
+
+func Track(event string, data map[string]interface{}) {
+
+	var siftErr SiftError{}
+
+	payload := map[string]interface{}{}
+	result := map[string]interface{}{}
+
+	if ApiKey != "" {
+		payload["$api_key"] = ApiKey
+	}
+
+	if UserId != "" {
+		payload["$user_id"] = UserId
+	}
+
+	if SessionId != "" {
+		payload["$session_id"] = SessionId
+	}
+
+	for key, value := range data {
+		payload[key] = value
+	}
+
+	resp, err := napping.Post(siftEndpoint, &payload, &result, &siftErr)
+
+	if resp.Status() == 200 && err == nil {
+		fmt.Printf("%v\n", result)
+	} else {
+		fmt.Printf("%v\n", siftErr)
+	}
+}
